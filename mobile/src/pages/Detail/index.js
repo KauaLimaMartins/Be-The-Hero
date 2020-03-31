6586b65c7dac7379f0344d097d869a6c7df4a305
KauaLimaMartins <kaualimadev0601@gmail.com>
2020-03-30 22:40:37 -0300

    mobile

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,72 +1,60 @@
-import React from "react";
-import { useNavigation, useRoute } from "@react-navigation/native";
-import { Feather } from "@expo/vector-icons";
-import { View, Text, Image, TouchableOpacity, Linking } from "react-native";
-import * as MailComposer from "expo-mail-composer";
+import React from 'react'
+import { Feather } from '@expo/vector-icons'
+import { useNavigation, useRoute } from '@react-navigation/native'
+import { Text, View, Image, TouchableOpacity, Linking } from 'react-native'
+import * as MailComposer from 'expo-mail-composer'
 
-import api from "../../services/api";
-import styles from "./styles";
-
-import logoImg from "../../assets/logo.png";
+import styles from './styles'
+import logoImg from '../../assets/logo.png'
 
 export default function Detail() {
-  const navigation = useNavigation();
-  const route = useRoute();
+  const navigation = useNavigation()
+  const route = useRoute()
 
-  const incident = route.params.incident;
-  const message = `Olá ${
-    incident.name
-  }, estou entrando em contato pois gostaria de ajudar no caso "${
-    incident.title
-  }" com o valor de ${Intl.NumberFormat("pt-BR", {
-    style: "currency",
-    currency: "BRL"
-  }).format(incident.value)}`;
+  const incident = route.params.incident
 
-  function navigateToIncidents() {
-    navigation.goBack();
+  const message = `Olá, ${incident.name}, estou entrando em contato pois gostaria de ajudar no caso "${incident.title}" com o valor de ${Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}.`
+
+  function navigateBack() {
+    navigation.goBack()
   }
 
   function sendMail() {
     MailComposer.composeAsync({
       subject: `Herói do caso: ${incident.title}`,
       recipients: [incident.email],
-      body: message
-    });
+      body: message,
+    })
   }
 
   function sendWhatsapp() {
-    Linking.openURL(
-      `whatsapp://send?phone=55${incident.whatsapp}&text=${message}`
-    );
+    Linking.openURL(`whatsapp://send?phone=${incident.whatsapp}&text=${message}`)
   }
 
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <TouchableOpacity onPress={navigateToIncidents}>
-          <Feather name="arrow-left" size={28} color="#E02041" />
-        </TouchableOpacity>
-
         <Image source={logoImg} />
+
+        <TouchableOpacity onPress={navigateBack}>
+          <Feather name='arrow-left' size={28} color='#E02041' />
+        </TouchableOpacity>
       </View>
 
       <View style={styles.incident}>
-        <Text style={[styles.incidentProperty, { marginTop: 0 }]}>ONG:</Text>
-        <Text style={styles.incidentValue}>
-          {incident.name} de {incident.city}/{incident.uf}
-        </Text>
+          <Text style={[styles.incidentProperty, { marginTop: 0, }]}>ONG:</Text>
+  <Text style={styles.incidentValue}>{incident.name} de {incident.city},{' ' + incident.uf}</Text>
 
-        <Text style={styles.incidentProperty}>CASO:</Text>
-        <Text style={styles.incidentValue}>{incident.title}</Text>
+          <Text style={styles.incidentProperty}>CASO:</Text>
+          <Text style={styles.incidentValue}>{incident.title}</Text>
 
-        <Text style={styles.incidentProperty}>VALOR:</Text>
-        <Text style={styles.incidentValue}>
-          {Intl.NumberFormat("pt-BR", {
-            style: "currency",
-            currency: "BRL"
-          }).format(incident.value)}
-        </Text>
+          <Text style={styles.incidentProperty}>VALOR:</Text>
+          <Text style={styles.incidentValue}> 
+            {Intl.NumberFormat('pt-BR', { 
+              style: 'currency', 
+              currency: 'BRL' 
+            }).format(incident.value)}
+          </Text>
       </View>
 
       <View style={styles.contactBox}>
@@ -74,16 +62,17 @@ export default function Detail() {
         <Text style={styles.heroTitle}>Seja o herói desse caso.</Text>
 
         <Text style={styles.heroDescription}>Entre em contato:</Text>
-
+        
         <View style={styles.actions}>
           <TouchableOpacity style={styles.action} onPress={sendWhatsapp}>
-            <Text style={styles.actionText}>Whatsapp</Text>
+            <Text style={styles.actionText}>WhatsApp</Text>
           </TouchableOpacity>
+          
           <TouchableOpacity style={styles.action} onPress={sendMail}>
             <Text style={styles.actionText}>E-mail</Text>
           </TouchableOpacity>
         </View>
       </View>
     </View>
-  );
+  )
 }
--- mobile/src/pages/Incidents/styles.js
+++ mobile/src/pages/Incidents/styles.js
@@ -1,75 +1,76 @@
-import { StyleSheet } from "react-native";
-import Constants from "expo-constants";
+import { StyleSheet } from 'react-native'
+import Constants from 'expo-constants'
 
 export default StyleSheet.create({
   container: {
     flex: 1,
     paddingHorizontal: 24,
-    paddingTop: Constants.statusBarHeight + 20
+    paddingTop: Constants.statusBarHeight + 20,
   },
 
   header: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between"
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
   },
 
   headerText: {
     fontSize: 15,
-    color: "#737380"
+    color: '#737380',
   },
 
   headerTextBold: {
-    fontWeight: "bold"
+    fontWeight: 'bold',
   },
 
   title: {
-    fontSize: 30,
-    marginBottom: 16,
-    marginTop: 48,
-    color: "#13131a",
-    fontWeight: "bold"
+    fontSize: 25,
+    marginBottom: 5,
+    marginTop: 31,
+    color: '#1F2127',
+    fontWeight: 'bold',
   },
 
   description: {
-    fontSize: 16,
+    fontSize: 14,
     lineHeight: 24,
-    color: "#737380"
+    color: '#737380',
+    marginBottom: 10,
   },
 
-  incidentList: {
-    marginTop: 32
+  incidentlist: {
+    marginTop: 32,
   },
 
   incident: {
     padding: 24,
     borderRadius: 8,
-    backgroundColor: "#FFF",
-    marginBottom: 16
+    backgroundColor: '#FFF',
+    marginBottom: 16,
   },
-
+  
   incidentProperty: {
     fontSize: 14,
-    color: "#41414d",
-    fontWeight: "bold"
+    color: '#41414D',
+    fontWeight: 'bold',
   },
 
   incidentValue: {
     marginTop: 8,
     fontSize: 15,
     marginBottom: 24,
-    color: "#737380"
+    color: '#737380',
   },
 
   detailsButton: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center"
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
 
   detailsButtonText: {
-    color: "#E02041",
+    color: '#E02041',
     fontSize: 15,
-    fontWeight: "bold"
+    fontWeight: 'bold',
   }
-});
+})
